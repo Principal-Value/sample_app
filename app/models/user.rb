c8758369f5b628c604790d89c60db1e061de2fae
Ikilonchic <ikilonchic@gmail.com>
2020-12-14 02:45:59 +0200

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 8, maximum: 32 }
+    validates :password, presence: true, length: { minimum: 8, maximum: 32 }, allow_nil: true
 
     def remember
         self.remember_token = User.new_token
