3c623031ae55a6aad4ef976a1f9eee59728c482e
Ikilonchic <ikilonchic@gmail.com>
2020-12-17 23:33:48 +0200

    Add password resets & email configuration

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,15 +1,30 @@
 require 'test_helper'
+require 'base64'
 
 class UserMailerTest < ActionMailer::TestCase
+  def setup
+    @user = users(:michael)
+    @user.activation_token = User.new_token
+    @user.reset_token = User.new_token
+    @activation_mail = UserMailer.account_activation(@user)
+    @reset_mail = UserMailer.password_reset(@user)
+  end
+
+
   test "account_activation" do
-    user = users(:michael)
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI::escape(user.email), mail.body.encoded
+    assert_equal "Account activation", @activation_mail.subject
+    assert_equal [@user.email], @activation_mail.to
+    assert_equal ["noreply@example.com"],  @activation_mail.from
+    assert_match @user.name,               @activation_mail.body.encoded
+    assert_match @user.activation_token,   @activation_mail.body.encoded
+    assert_match CGI::escape(@user.email), @activation_mail.body.encoded
+  end
+
+  test "password_reset" do
+    assert_equal "Password reset", @reset_mail.subject
+    assert_equal [@user.email], @reset_mail.to
+    assert_equal ["noreply@example.com"],  @reset_mail.from
+    assert_match @user.reset_token,        @reset_mail.body.encoded
+    assert_match CGI::escape(@user.email), @reset_mail.body.encoded
   end
 end
