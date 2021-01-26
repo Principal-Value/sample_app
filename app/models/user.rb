b90b922311282707ef60181d0e3350a179b24a79
Ikilonchic <ikilonchic@gmail.com>
2021-01-27 01:55:12 +0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ActiveRecord::Base
 
     has_many :microposts, dependent: :destroy
 
+    has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+ 
+
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
     before_save   :downcase_email
     before_create :create_activation_digest
 
@@ -16,7 +27,10 @@ class User < ActiveRecord::Base
     validates :password, presence: true, length: { minimum: 8, maximum: 32 }, allow_nil: true
 
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
     end
 
     def remember
@@ -65,6 +79,18 @@ class User < ActiveRecord::Base
 
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
+    end
+
+    def follow(other_user)
+        active_relationships.create(followed_id: other_user.id)
+    end
+
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
     end
 
     private
